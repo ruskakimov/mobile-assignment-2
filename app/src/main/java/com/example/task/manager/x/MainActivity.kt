fcf9bce143257502a9c19ebf16f03af4564ceb20
Rustem Kakimov <ruskakimov@gmail.com>
2024-07-29 18:59:02 -0400

    Inflate options menu

--- app/src/main/java/com/example/task/manager/x/MainActivity.kt
+++ app/src/main/java/com/example/task/manager/x/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.task.manager.x
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,36 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_add -> {
+                // TODO: Handle adding a new TODO item
+                true
+            }
+            R.id.action_hide_completed -> {
+                // TODO: Handle hiding completed tasks
+                true
+            }
+            R.id.action_show_completed -> {
+                // TODO: Handle showing completed tasks
+                true
+            }
+            R.id.action_delete_completed -> {
+                // TODO: Handle deleting completed tasks
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
